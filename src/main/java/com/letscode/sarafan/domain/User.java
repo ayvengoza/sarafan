1cfbbe81c782eb169c1cc8f6fe549013ce0c6d4e
ayvengoza <ayvengoza@gmail.com>
2020-01-14 17:51:32 +0200

    #6 vue.js auth

--- src/main/java/com/letscode/sarafan/domain/User.java
+++ src/main/java/com/letscode/sarafan/domain/User.java
@@ -1,18 +1,20 @@
 package com.letscode.sarafan.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "usr")
 @Setter
 @Getter
-public class User {
+public class User implements Serializable {
     @Id
     private String id;
     private String name;
@@ -20,5 +22,6 @@ public class User {
     private String email;
     private String gender;
     private String locale;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastVisit;
 }
